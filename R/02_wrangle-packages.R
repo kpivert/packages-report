32c686c0c146554eb79c5d4be8977723879174f1
Kurtis Pivert <kurtis.pivert@gmail.com>
2018-02-01 11:46:23 -0800

    Saved add-on packages dataframe

--- R/02_wrangle-packages.R
+++ R/02_wrangle-packages.R
@@ -1,6 +1,11 @@
 ## create a data frame from data/installed-packages.csv
 ## with, e.g., readr::read_csv() or read.csv()
 
+# 1. Load Packages --------------------------------------------------------
+
+  require(tidyverse)
+  df <- readr::read_csv("data/installed-packages.csv")
+
 ## filter out packages in the default library
 ## keep variables Package and Built
 ## if you use dplyr, code like this will work:
@@ -8,8 +13,12 @@ apt <- ipt %>%
   filter(LibPath == .libPaths()[1]) %>%
   select(Package, Built)
 
+  df_not_default <- df %>% filter(LibPath == LibPath[2]) %>%
+    select(Package, Built)
+
 ## write this new, smaller data frame to data/add-on-packages.csv
 
+  write_csv(x = df_not_default, path = "data/add-on-packages.csv")
 ## make a frequency table of package by the version in Built
 ## if you use dplyr, code like this will work:
 apt_freqtable <- apt %>%
