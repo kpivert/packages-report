32e7352c47ea2b05575d6e1db138d2c5866d3abb
Kurtis Pivert <kurtis.pivert@gmail.com>
2018-02-01 11:38:49 -0800

    Added new dataset of my packages

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,16 @@
 ## deja vu from yesterday!
 
+
+# 1. Load Packages  -------------------------------------------------------
+
+  require(tidyverse)
+  require(here)
+
 ## create a data frame of your installed packages
+
+  df <- installed.packages() %>%
+    as.tibble()
+
 
 ## keep the variables
 ##   * Package
@@ -9,7 +19,11 @@
 ##   * Priority
 ##   * Built
 
+  df <- df %>% select(Package, LibPath, Version, Priority, Built)
+
 ## write it to data/installed-packages.csv
+
+  write_csv(x = df, path = "data/installed-packages.csv")
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
